689e1213b2239df5c31ee9c1adc62879844326ef
Phillip Vose <phillip.vose@onthebeach.co.uk>
2014-10-16 10:35:10 +0100

    Removed previous changes as they didn't really work and were over-complicated.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,29 +1,14 @@
 class BowlingLineScorer
   def score(line)
     total_score = 0
-    spare = false
-    strike = false
 
     line.each do |frame|
-      if spare
-        total_score += frame.first
-        spare = false
-      end
       num_of_throws = frame.length
       frame_score = frame.reduce(:+)
-      total_score += frame_score
 
-      spare = spare_check(frame_score, num_of_throws)
+      total_score += frame_score
     end
 
     return total_score
-  end
-
-  def spare_check(frame_score, num_of_throws)
-    if frame_score == 10 && num_of_throws > 1
-      return true
-    else
-      return false
-    end
   end
 end
